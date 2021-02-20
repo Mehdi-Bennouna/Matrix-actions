d8b61657a023aa489247ce090f9e7c2ceb876d35
BENNOUNA Mehdi <Bennouna.mehdi@hotmail.com>
2021-02-20 23:27:05 +0100

    added proper menus selection and cleaned up a bit

--- project_real.c
+++ project_real.c
@@ -5,6 +5,7 @@
 
 typedef struct l_choix
 {
+    int nombre_choix_valide;
     int nombre_choix;
     char **liste;
 } liste_choix;
@@ -51,6 +52,8 @@ HANDLE wHnd; // Handle to write to the console.
 HANDLE rHnd; // Handle to read from the console.
 
 void afficher_cadre(liste_choix choix);
+void print_menu(WINDOW *fenetre_menu, int selection, liste_choix choix);
+int afficher_menu(liste_choix choix);
 
 int main()
 {
@@ -70,9 +73,41 @@ int main()
     cbreak();
     curs_set(0);
 
-    liste_choix choix = {5, liste_choix_1};
+    liste_choix choix = {5, 5, liste_choix_0}; //initialisation au menu principal
+    int n_liste_choix = 0;
 
-    afficher_cadre(choix);
+    while (n_liste_choix != 4)
+    {
+        n_liste_choix = afficher_menu(choix);
+
+        if (n_liste_choix == 1)
+        {
+            choix.liste = liste_choix_1;
+            choix.nombre_choix = 7;
+            choix.nombre_choix_valide = 7;
+
+            n_liste_choix = afficher_menu(choix);
+        }
+
+        if (n_liste_choix == 2)
+        {
+            choix.liste = liste_choix_2;
+            choix.nombre_choix = 7;
+            choix.nombre_choix_valide = 7;
+
+            n_liste_choix = afficher_menu(choix);
+        }
+
+        if (n_liste_choix == 3)
+        {
+            choix.liste = liste_choix_3;
+            choix.nombre_choix = 9;
+            choix.nombre_choix_valide = 9;
+
+            n_liste_choix = afficher_menu(choix);
+        }
+    }
+
     wgetch(stdscr);
 
     endwin();
@@ -83,4 +118,77 @@ void afficher_cadre(liste_choix choix)
     box(stdscr, 0, 0);
     mvprintw(0, (50 - strlen(choix.liste[0]) / 2), " %s ", choix.liste[0]);
     refresh();
+}
+
+int afficher_menu(liste_choix choix)
+{
+    afficher_cadre(choix);
+    int LARGEUR = 51;
+    int HAUTEUR = 20;
+
+    int debutx = 25;
+    int debuty = 6;
+
+    WINDOW *fenetre_menu;
+    int selection = 2;
+    int choice = 0;
+    int c;
+
+    debutx = 25;
+    debuty = 1;
+    fenetre_menu = newwin(HAUTEUR, LARGEUR, debuty, debutx);
+    keypad(fenetre_menu, TRUE);
+
+    print_menu(fenetre_menu, selection, choix);
+    while (1)
+    {
+        c = wgetch(fenetre_menu);
+        switch (c)
+        {
+        case KEY_UP:
+            if (selection == 2)
+                selection = choix.nombre_choix;
+            else
+                --selection;
+            break;
+        case KEY_DOWN:
+            if (selection == choix.nombre_choix)
+                selection = 2;
+            else
+                ++selection;
+            break;
+        case 10:
+            choice = selection;
+            break;
+        default:
+            refresh();
+            break;
+        }
+        print_menu(fenetre_menu, selection, choix);
+        if (choice != 0) /* sortie de boucle */
+            break;
+    }
+    refresh();
+
+    return choice - 1;
+}
+
+void print_menu(WINDOW *fenetre_menu, int selection, liste_choix choix)
+{
+    int x, y, i;
+
+    y = 6;
+    for (i = 1; i < choix.nombre_choix; ++i)
+    {
+        if (selection == i + 1) /* Highlight the present choice */
+        {
+            wattron(fenetre_menu, A_REVERSE);
+            mvwprintw(fenetre_menu, y, (25 - strlen(choix.liste[i]) / 2), "%s", choix.liste[i]);
+            wattroff(fenetre_menu, A_REVERSE);
+        }
+        else
+            mvwprintw(fenetre_menu, y, (25 - strlen(choix.liste[i]) / 2), "%s", choix.liste[i]);
+        ++y;
+    }
+    wrefresh(fenetre_menu);
 }
